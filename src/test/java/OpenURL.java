3b4361e7de83dd116da1b2d5b9880646670726c7
smrutisdet <smmohapatra011@gmail.com>
2025-02-09 20:39:31 +0530

    Selenium4Demo project set up and WebDriver and driver relationship Demo

---
+++ src/test/java/OpenURL.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class OpenURL {
+    public static void main(String[] args) {
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://www.google.com/");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+}
