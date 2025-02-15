110209dc52c3f00e7cdf0f64c1a9d96c63ecda90
smrutisdet <smmohapatra011@gmail.com>
2025-02-15 17:01:20 +0530

    LocatorInto

---
+++ src/test/java/LocatorIntoduction.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LocatorIntoduction {
+    public static void main(String[] args) {
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://www.google.com/");
+        System.out.println(driver.getTitle());
+      WebElement searchBar = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+       System.out.println("is search bar displayed? "+searchBar.isDisplayed());
+        driver.quit();
+    }
+}
